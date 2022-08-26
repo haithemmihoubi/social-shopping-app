a0b503700e4d8fd58b4547183b93a4c80b46bea1
J.Boughattas <boughattasjassem@outlook.com>
2022-08-26 22:41:23 +0100

    add Error page layout, styling & animation

--- frontend/src/components/Error/Error.js
+++ frontend/src/components/Error/Error.js
@@ -1,16 +1,38 @@
+import {motion, AnimatePresence} from 'framer-motion';
+import {Link} from "react-router-dom";
+
 export default function Error() {
 
   return (
     <div
-      className="h-screen flex justify-center pt-20 text-center select-none"
+      className="h-screen flex justify-center pt-20 text-center select-none overflow-hidden"
       style={{
         color: '#F5C8C6',
         textShadow: '0 0 15px rgba(0,0,0,0.2)'
       }}>
-      <div>
-        <h2 className="text-9xl uppercase">Error</h2>
-        <p className="text-2xl">Request Failed</p>
-      </div>
+      <AnimatePresence>
+        <div>
+          <motion.h2
+            initial={{scale:0,opacity:0,y:-100}} animate={{scale:1,opacity:1,y:0}}
+            transition={{delay: .4, duration: .4, type: 'spring'}}
+            className="text-9xl uppercase">Error</motion.h2>
+          <motion.p
+            initial={{scale:0,opacity:0,y:10}} animate={{scale:1,opacity:1,y:0}}
+            transition={{delay: .6, duration: .4, type: 'spring'}}
+            className="text-2xl">Request Failed</motion.p>
+          <motion.div
+            initial={{scale: 0, opacity: 0, rotate: -90}} animate={{scale:1,opacity:1,rotate: 0}}
+            transition={{delay:1,duration:.4,type:'spring'}}
+          >
+            <motion.button
+              whileHover={{scale: 1.05}} whileTap={{scale: 0.95}}
+              className="px-4 py-2 bg-[#F5C8C6] text-red-500 m-10 rounded-xl shadow-md"
+            >
+              <Link to="/" replace>Go to Home Page</Link>
+            </motion.button>
+          </motion.div>
+        </div>
+      </AnimatePresence>
     </div>
   );
 }
